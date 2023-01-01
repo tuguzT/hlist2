ef0cb25f1b22e72c0d004bbd56c95ce6464a2d7f
tuguzT <timurka.tugushev@gmail.com>
2023-01-01 21:54:15 +0300

    Add operation to reverse heterogenous list

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -1,7 +1,8 @@
 //! Provides operations defined for heterogenous lists.
 
-pub use self::{append::Append, extend::Extend, prepend::Prepend};
+pub use self::{append::Append, extend::Extend, prepend::Prepend, reverse::Reverse};
 
 mod append;
 mod extend;
 mod prepend;
+mod reverse;
---
+++ src/ops/reverse.rs
@@ -0,0 +1,57 @@
+use crate::{Cons, HList, Nil};
+
+/// Reverse elements of the heterogenous list.
+///
+/// New heterogenous list will contain all elements of the old one
+/// with the opposite order.
+pub trait Reverse: HList {
+    /// Type of new heterogenous list with the opposite order of elements.
+    type Output: HList;
+
+    /// Reverses elements of the heterogenous list.
+    fn reverse(self) -> Self::Output;
+}
+
+impl<T> Reverse for T
+where
+    T: Rewind<Nil>,
+{
+    type Output = T::Output;
+
+    fn reverse(self) -> Self::Output {
+        self.rewind(Nil)
+    }
+}
+
+pub trait Rewind<Done>: HList
+where
+    Done: HList,
+{
+    type Output: HList;
+
+    fn rewind(self, done: Done) -> Self::Output;
+}
+
+impl<Done> Rewind<Done> for Nil
+where
+    Done: HList,
+{
+    type Output = Done;
+
+    fn rewind(self, done: Done) -> Self::Output {
+        done
+    }
+}
+
+impl<Done, Next, Tail> Rewind<Done> for Cons<Next, Tail>
+where
+    Done: HList,
+    Tail: Rewind<Cons<Next, Done>>,
+{
+    type Output = Tail::Output;
+
+    fn rewind(self, done: Done) -> Self::Output {
+        let Cons(next, tail) = self;
+        tail.rewind(Cons(next, done))
+    }
+}
