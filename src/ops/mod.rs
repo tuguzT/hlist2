67e40a553349e2fbcfdd42e47a256048b9c7548c
tuguzT <timurka.tugushev@gmail.com>
2023-01-01 22:23:00 +0300

    Add operation to remove the first element from the list

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -1,8 +1,11 @@
 //! Provides operations defined for heterogenous lists.
 
-pub use self::{append::Append, extend::Extend, prepend::Prepend, reverse::Reverse};
+pub use self::{
+    append::Append, extend::Extend, pop_front::PopFront, prepend::Prepend, reverse::Reverse,
+};
 
 mod append;
 mod extend;
+mod pop_front;
 mod prepend;
 mod reverse;
---
+++ src/ops/pop_front.rs
@@ -0,0 +1,36 @@
+use crate::{Cons, HList};
+
+/// Remove the first element from the heterogenous list.
+///
+/// New element will be removed from the beginning of the heterogenous list,
+/// resulting in tuple of new heterogenous list and removed element.
+///
+/// # Examples
+///
+/// ```
+/// use hlist2::{hlist, ops::PopFront};
+///
+/// let hlist = hlist!(1, 2.0, true);
+/// let (hlist, elem) = hlist.pop_front();
+/// assert_eq!(hlist, hlist!(2.0, true));
+/// assert_eq!(elem, 1);
+/// ```
+pub trait PopFront {
+    /// Tuple of new heterogenous list after removing the first element and removed element.
+    type Output;
+
+    /// Removes the first element from the heterogenous list.
+    fn pop_front(self) -> Self::Output;
+}
+
+impl<Head, Tail> PopFront for Cons<Head, Tail>
+where
+    Tail: HList,
+{
+    type Output = (Tail, Head);
+
+    fn pop_front(self) -> Self::Output {
+        let Cons(head, tail) = self;
+        (tail, head)
+    }
+}
