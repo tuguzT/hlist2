df325a30d23f1bc06658b174edcc3cde61edeb9f
tuguzT <timurka.tugushev@gmail.com>
2023-09-02 09:18:33 +0300

    Rename type access traits

--- src/ops/mod.rs
+++ src/ops/mod.rs
@@ -5,13 +5,12 @@ pub use self::{
     extend::Extend,
     flatten::Flatten,
     fold::{Fold, FoldFn, Folder, RFold},
-    get::Get,
     index::{Dec, Here, Inc, Index, There},
     map::{Map, MapFn, Mapper},
     pop::Pop,
     pop_front::PopFront,
     prepend::Prepend,
-    remove::Remove,
+    provide::{Provide, ProvideOnce},
     reverse::Reverse,
     to_ref::ToRef,
     unzip::Unzip,
@@ -22,13 +21,12 @@ mod append;
 mod extend;
 mod flatten;
 mod fold;
-mod get;
 mod index;
 mod map;
 mod pop;
 mod pop_front;
 mod prepend;
-mod remove;
+mod provide;
 mod reverse;
 mod to_ref;
 mod unzip;
